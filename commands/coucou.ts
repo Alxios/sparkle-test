a2e536c63c3d219cd876a395fd849b33249303b1
Alexandre G. <alexgaspar@hotmail.fr>
2026-04-20 19:50:54 +0200

    initialize repository from sparkle

---
+++ commands/coucou.ts
@@ -0,0 +1,12 @@
+export default createCommand({
+  alias: ["message"],
+  params: {
+    message: z.string("Tu dois fournir un message après !coucou"),
+  },
+  run: async (payload, ctx) => {
+    const { message } = payload.params
+    await ctx.twitch.sendMessage({
+      message: `Coucou: ${message}`
+    })
+  },
+})
